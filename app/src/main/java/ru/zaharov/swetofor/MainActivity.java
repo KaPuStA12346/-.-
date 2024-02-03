08328cd02fefe427a9b39be5c252e94d1cfde685
KaPuStA12346 <lega46664@gmail.com>
2024-02-03 17:18:52 +0500

    акончил светофор

--- app/src/main/java/ru/zaharov/swetofor/MainActivity.java
+++ app/src/main/java/ru/zaharov/swetofor/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout b_1;
     private LinearLayout b_2, b_3;
     private Button btn;
+    private Boolean  isStarted = false;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +24,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickStart(View view) {
+        if (!isStarted) {
+            isStarted = true;
+            btn.setText("STOP");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (isStarted) {
+                        counter++;
+                        switch (counter) {
+                            case 1:
+                                b_1.setBackgroundColor(getResources().getColor(R.color.green));
+                                b_2.setBackgroundColor(getResources().getColor(R.color.gray));
+                                b_3.setBackgroundColor(getResources().getColor(R.color.gray));
+                                break;
+                            case 2:
+                                b_1.setBackgroundColor(getResources().getColor(R.color.gray));
+                                b_2.setBackgroundColor(getResources().getColor(R.color.yellow));
+                                b_3.setBackgroundColor(getResources().getColor(R.color.gray));
+                                break;
+                                case 3:
+                                    b_1.setBackgroundColor(getResources().getColor(R.color.gray));
+                                    b_2.setBackgroundColor(getResources().getColor(R.color.gray));
+                                    b_3.setBackgroundColor(getResources().getColor(R.color.red));
+                            counter= 0;
+                            break;
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }).start();
+        } else{
+            isStarted = false;
+            btn.setText("START");
+            b_1.setBackgroundColor(getResources().getColor(R.color.gray));
+            b_2.setBackgroundColor(getResources().getColor(R.color.gray));
+            b_3.setBackgroundColor(getResources().getColor(R.color.gray));
+            counter= 0;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isStarted= false;
     }
 }
